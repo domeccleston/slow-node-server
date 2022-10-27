13878d6497f2b4b2c71c166ace255e30acb67599
Dom Eccleston <31101792+domeccleston@users.noreply.github.com>
2022-10-27 11:15:14 +0100

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -4,6 +4,6 @@ const timeout = 11000
 
 createServer(async (req, res) => {
   await new Promise((resolve) => setTimeout(resolve, timeout));
-  res.write(`Sending a response after $${timeout / 1000} seconds.`);
+  res.write(`Sending a response after ${timeout / 1000} seconds.`);
   res.end();
 }).listen(process.env.PORT);
