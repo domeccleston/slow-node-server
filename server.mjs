c1a8e33a92b4e44da5bacdbf2e45ee5cdd3356e1
Dom Eccleston <31101792+domeccleston@users.noreply.github.com>
2022-10-27 11:12:09 +0100

    Update server.mjs
    add async

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import { createServer } from 'http';
 
 const timeout = 11000
 
-createServer((req, res) => {
+createServer(async (req, res) => {
   await new Promise((resolve) => setTimeout(resolve, timeout));
   res.write(`Sending a response after $${timeout / 1000} seconds.`);
   res.end();
