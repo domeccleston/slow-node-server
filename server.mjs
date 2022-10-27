bfbdd4f18afad5a857d33300d41b11fdd64a1c25
Dom Eccleston <31101792+domeccleston@users.noreply.github.com>
2022-10-27 11:10:55 +0100

    Update server.mjs
    add timeout

--- server.mjs
+++ server.mjs
@@ -1,6 +1,9 @@
 import { createServer } from 'http';
 
+const timeout = 11000
+
 createServer((req, res) => {
-  res.write('Hello World!');
+  await new Promise((resolve) => setTimeout(resolve, timeout));
+  res.write(`Sending a response after $${timeout / 1000} seconds.`);
   res.end();
 }).listen(process.env.PORT);
